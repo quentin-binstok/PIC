9517965a5201ea3a5c08da602b65e6e945d7530d
Neo <neo.collin@student.uliege.be>
2026-02-03 16:50:27 +0100

    Interpolate + semiLagrangian

---
+++ src/utils.cpp
@@ -0,0 +1,24 @@
+#include <cstdlib>
+#include <istream>
+#include <ostream>
+#include <string>
+#include <cinttypes>
+#include "data.hpp"
+#include "utils.hpp"
+
+float interpolate_bilinear(float x, float y, int x1, int y1, float q11, 
+                           float q21, float q12, float q22,
+                           float Dx, float Dy) {
+    // x1,y1 is the bottom-left corner of the cell, Dx and Dy are the cell sizes
+    // q11, q21, q12, q22 are the values at the corners (11 is bottom-left)
+    
+    float dx = x - x1;
+    float dy = y - y1;
+    float value = (1 - (dx/Dx) - (dy/Dy) + (dx*dy)/(Dx*Dy)) * q11
+                + (dx/Dx - (dx*dy)/(Dx*Dy)) * q21
+                + (dy/Dy - (dx*dy)/(Dx*Dy)) * q12
+                + ((dx*dy)/(Dx*Dy)) * q22;
+    return value;
+}
+
+
--- src/utils.hpp
+++ src/utils.hpp
@@ -5,4 +5,8 @@
 #define LOG_WARN(log_file, msg) log_file << "[WARN] " << msg << std::endl;
 #define LOG_ERR(log_file, msg) log_file << "[ERROR] " << msg << std::endl;
 
+float interpolate_bilinear(int x, int y, int x1, int y1, float q11, 
+                           float q21, float q12, float q22,
+                           float Dx, float Dy);
+
 #endif
